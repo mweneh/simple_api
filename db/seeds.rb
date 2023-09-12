5e6680ea95080f921f7277c276f1e7d77c936a56
David <davidongaroh@gmail.com>
2023-09-12 12:06:28 +0300

    add seeds with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+# Generate and print 10 random names
+10.times do
+  people = Person.create(Faker::Name.name)
+end
